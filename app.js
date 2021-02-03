b8931be20eaf53dfb5bd9d0962a29248db3c19f3
Steven Ly <ly.steven16@gmail.com>
2021-02-03 01:37:44 -0600

    Installed inquirer and first prompt

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 // console.log(profileDataArgs);
@@ -21,8 +21,18 @@ const [name, github] = profileDataArgs;
 // };
 
  
- fs.writeFile('./index.html', generatePage(name, github), err => {
-     if (err) throw new Error(err);
+//  fs.writeFile('./index.html', pageHTML, err => {
+//      if (err) throw err;
 
-     console.log('Portfolio complete! Check out index.html to see the output!');
- });
+//      console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
