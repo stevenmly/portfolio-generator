3cbcd52d2bab6fa326d4c5212429cdc9bcdd94ff
Steven Ly <ly.steven16@gmail.com>
2021-02-02 20:37:07 -0600

    generated html file

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('=============');
+const [name, github] = profileDataArgs;
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// console.log(profileDataArgs);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-printProfileData(profileDataArgs);
+//   console.log('=============');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+ 
+ fs.writeFile('./index.html', generatePage(name, github), err => {
+     if (err) throw new Error(err);
+
+     console.log('Portfolio complete! Check out index.html to see the output!');
+ });
